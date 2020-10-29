4fc4435136cb44546ce31b89327b7635bf5c7dab
Yoshimasa Kataoka <y.kataoka.824@gmail.com>
2020-10-29 09:24:29 +0900

    Format tf files

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,20 +1,20 @@
 provider "azurerm" {
-    version = "~>2.0"
-    features {}
+  version = "~>2.0"
+  features {}
 }
 
 terraform {
-    backend "azurerm" {
-        resource_group_name = "tools"
-        storage_account_name = "ykataokashare"
-        container_name = "tfstate"
-        key = "todoservice.dev.tfstate"
-    }
+  backend "azurerm" {
+    resource_group_name  = "tools"
+    storage_account_name = "ykataokashare"
+    container_name       = "tfstate"
+    key                  = "todoservice.dev.tfstate"
+  }
 }
 
 module "common" {
-    source = "../../modules"
+  source = "../../modules"
 
-    environment = "dev"
-    location = "japaneast"
+  environment = "dev"
+  location    = "japaneast"
 }
