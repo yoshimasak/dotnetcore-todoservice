4fe88ec6d4af6be05c72aeee6bf15ea31a0106f7
Yoshimasa Kataoka <y.kataoka.824@gmail.com>
2020-10-29 09:23:09 +0900

    Add variables

---
+++ envs/prd/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+    version = "~>2.0"
+    features {}
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name = "tools"
+        storage_account_name = "ykataokashare"
+        container_name = "tfstate"
+        key = "todoservice.prd.tfstate"
+    }
+}
+
+module "common" {
+    source = "../../modules"
+
+    environment = "prd"
+    location = "japaneast"
+}
